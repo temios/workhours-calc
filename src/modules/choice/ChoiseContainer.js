8a1e51faf16a045d56efb4e6760e771bb6429c73
temios <drilnmain@gmail.com>
2018-12-30 20:17:38 +0300

    add mock

--- src/modules/choice/ChoiseContainer.js
+++ src/modules/choice/ChoiseContainer.js
@@ -1,13 +1,24 @@
 import * as React from 'react'
 
 class ChoiceContainer extends React.Component {
+  constructor (props, context) {
+    super(props)
+    let parts = []
+    fetch('/mock.json').then((response) => {
+      return response.json()
+    }).then((data) => {
+      parts = data
+      console.log(parts)
+    })
+  }
+
   render () {
     return (
       <div>
-choise
+        choise
       </div>
     )
   }
 }
 
-export default ChoiceContainer;
+export default ChoiceContainer
